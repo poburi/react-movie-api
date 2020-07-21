f2a4a8611fe8d68af96ed9f009216ee6e2f44c5c
poburi <shohom35@naver.com>
2020-07-21 09:55:04 +0900

    first commit

---
+++ src/components/ui/Search.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState("");
+  const onChange = (q) => {
+    setText(q);
+    getQuery(q);
+  };
+  return (
+    <section className="search">
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="출연진 검색"
+          autoFocus
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Search;
